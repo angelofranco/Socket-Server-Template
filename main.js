005da79c88090c1fda632071e350990a18d38ec6
Angelo <angelo.franco@gmail.com>
2026-05-27 18:26:55 -0300

    bugfix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const path = require('path');
 
 app.use(express.static("public"));
 // require("dotenv").config();
